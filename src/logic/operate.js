c420166011f99784de6e04aaf46d44d2db8b6576
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-30 20:20:33 -0500

    Update logic to return operate values as Strings

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -4,25 +4,21 @@ const operate = (numberOne, numberTwo, operation) => {
   const operand1 = Big(numberOne);
   const operand2 = Big(numberTwo);
 
-  try {
-    switch (operation) {
-      case '+/-':
-        return operand1.times(-1);
-      case '+':
-        return operand1.plus(operand2);
-      case '-':
-        return operand1.minus(operand2);
-      case 'X':
-        return operand1.times(operand2);
-      case '÷':
-        return operand1.div(operand2);
-      case '%':
-        return operand1.div(100);
-      default:
-        return '0';
-    }
-  } catch (error) {
-    return 'Error';
+  switch (operation) {
+    case '+/-':
+      return operand1.times(Big(-1));
+    case '+':
+      return operand1.plus(operand2);
+    case '-':
+      return operand1.minus(operand2);
+    case 'X':
+      return operand1.times(operand2);
+    case '÷':
+      return operand1.div(operand2);
+    case '%':
+      return operand1.div(100);
+    default:
+      return '0';
   }
 };
 
