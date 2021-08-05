26d3c6fb8a65b4f4548dfb7426019e9d5e30beef
sjcco <73748496+sjcco@users.noreply.github.com>
2021-08-05 00:08:58 -0500

    Refactor App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      // eslint-disable-next-line
-      operation: null,
-    };
+const App = () => {
+  const [memory, setMemory] = useState({
+    total: null,
+    next: null,
+    // eslint-disable-next-line
+    operation: null,
+  });
 
-    this.handleClick = btnName => {
-      this.setState(prevState => calculate(prevState, btnName));
-    };
-  }
+  const handleClick = btnName => {
+    setMemory(prevState => calculate(prevState, btnName));
+  };
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <>
-        <Display result={next || total || '0'} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  const { next, total } = memory;
+  return (
+    <>
+      <Display result={next || total || '0'} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
 export default App;
