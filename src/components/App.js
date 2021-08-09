750677b38b4a1d80ee64ca5f035b564e92f10047
sjcco <73748496+sjcco@users.noreply.github.com>
2021-08-09 18:09:24 -0500

    Add routes + home and quotes

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,20 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import ButtonPanel from './ButtonPanel';
-import Display from './Display';
-// eslint-disable-next-line no-unused-vars
-import calculate from '../logic/calculate';
+import Calculator from './calculator';
+import NavBar from './NavBar';
+import Home from './Home';
+import Quote from './Quote';
 
-const App = () => {
-  const [memory, setMemory] = useState({
-    total: null,
-    next: null,
-    // eslint-disable-next-line
-    operation: null,
-  });
-
-  const handleClick = btnName => {
-    setMemory(prevState => calculate(prevState, btnName));
-  };
-
-  const { next, total, operation } = memory;
-  return (
-    <div className="app skin">
-      <Display result={next || total || '0'} operation={'' || operation} />
-      <ButtonPanel clickHandler={handleClick} />
+const App = () => (
+  <BrowserRouter>
+    <NavBar />
+    <div className="content-container">
+      <Route exact path="/" component={Home} />
+      <Route path="/calculator" component={Calculator} />
+      <Route path="/quotes" component={Quote} />
     </div>
-  );
-};
+  </BrowserRouter>
+);
 
 export default App;
